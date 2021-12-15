6741436cffbccd6cd15d07abb527a5938d5cbf73
Tsukuyomi212 <nada.zelic@gmail.com>
2021-12-15 08:47:31 +0100

    refactor

---
+++ endpoints.js
@@ -0,0 +1,9 @@
+const endpoints = {
+  signup: 'https://www.phptravels.net/signup',
+  loginAfterSignup: 'https://www.phptravels.net/login/signup',
+  login: 'https://www.phptravels.net/login',
+  admin: 'https://phptravels.net/api/admin',
+  userDashboard: 'https://www.phptravels.net/account/dashboard',
+};
+
+module.exports = endpoints;
--- test/login.test.js
+++ test/login.test.js
@@ -1,9 +1,7 @@
-const { Builder, By, Key } = require('selenium-webdriver');
+const { Builder, By } = require('selenium-webdriver');
 const expect = require('chai').expect;
 const users = require('../users');
-
-const loginEndpoint = 'https://www.phptravels.net/login';
-const adminLoginEndpoint = 'https://phptravels.net/api/admin';
+const endpoints = require('../endpoints');
 
 describe('login test', function () {
   let driver;
@@ -16,21 +14,21 @@ describe('login test', function () {
   });
 
   it('successfully logs in a user', async function () {
-    await driver.get(loginEndpoint);
+    await driver.get(endpoints.login);
     await driver.findElement(By.name('email')).sendKeys(users.customer.email);
     await driver.findElement(By.name('password')).sendKeys(users.customer.password);
     await driver
       .findElement(By.xpath('/html/body/div[1]/div/div[2]/div[2]/div/form/div[3]/button'))
       .click();
 
-    const expectedUrl = 'https://www.phptravels.net/account/dashboard';
+    const expectedUrl = endpoints.userDashboard;
     const actualUrl = await driver.getCurrentUrl();
 
     expect(actualUrl).to.equal(expectedUrl);
   });
 
   it('successfully logs in an admin', async function () {
-    await driver.get(adminLoginEndpoint);
+    await driver.get(endpoints.admin);
     await driver.findElement(By.name('email')).sendKeys(users.admin.email);
     await driver.findElement(By.name('password')).sendKeys(users.admin.password);
     await driver.findElement(By.xpath('/html/body/div[1]/form[1]/button')).click();
